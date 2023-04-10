220feda1ce9b2fc2ce99764b88c1ecaab434b583
halukalp <haluk.allp@gmail.com>
2023-04-10 12:06:26 +0300

    bulut

--- src/main/java/com/universal_yazilim/bid/ysm/product_app/MicroserviceProductApplication.java
+++ src/main/java/com/universal_yazilim/bid/ysm/product_app/MicroserviceProductApplication.java
@@ -3,8 +3,10 @@ package com.universal_yazilim.bid.ysm.product_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class MicroserviceProductApplication
 {
 
