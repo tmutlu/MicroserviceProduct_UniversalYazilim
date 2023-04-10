245521dae67fefe84a18a5139eca4e14b24ef41a
tmutlu <tmutlu@iski.istanbul>
2023-04-10 09:24:13 +0300

    Initial commit

---
+++ src/test/java/com/universal_yazilim/bid/ysm/product_app/MicroserviceProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universal_yazilim.bid.ysm.product_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
